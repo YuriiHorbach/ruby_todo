7dedda37d3b22ff8634c5c4c2f805a0384fd0464
YuriiHorbach <gyrii84@gmail.com>
2020-10-06 23:37:33 +0300

    Fixied delete ability

--- db/migrate/20200904203922_create_todo_items.rb
+++ db/migrate/20200904203922_create_todo_items.rb
@@ -2,7 +2,8 @@ class CreateTodoItems < ActiveRecord::Migration[6.0]
   def change
     create_table :todo_items do |t|
       t.string :content
-      t.references :todo_list, null: false, foreign_key: true
+      t.references :todo_list, foreign_key: true
+      # t.references :todo_list, null: false, foreign_key: true
 
       t.timestamps
     end
